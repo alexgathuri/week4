d032d4ae3512401d2f9567726800695f4ec3245c
Alex <gathurialex4@gmail.com>
2021-01-14 11:51:57 +0300

    Final

--- java/script.js
+++ java/script.js
@@ -1,3 +1,4 @@
+
 var modal = document.getElementById("myModal");
 
 var btn = document.getElementById("myBtn");
@@ -18,11 +19,32 @@ window.onclick = function(event) {
   }
 }
 
-var size = documnet.getElementById("size").value;
-function submit(){
-    
-    documnet.getElementById("size").innerHTML=size;
-}
 
 
+
+
+    function display(){
+        var type = document.getElementById("type1").value;
+        var size = document.getElementById("size1").value;
+        var top = document.getElementById("top1").value;
+        var crust = document.getElementById("crust1").value;
+        var deliver = document.getElementById("deliver1").value;
+
+        
+        var size = parseInt (size);
+        var top = parseInt (top);
+        var crust = parseInt (crust);
+        var deliver = parseInt (deliver);
+        var total = size+top+crust+deliver;
+
+        alert("You have ordered "+ type );
+        alert("The price for "+type +"is Ksh"+ size);
+        alert("Crust price is ksh "+ crust);
+        alert("Topping price is ksh"+ top);
+        alert("Delivery price is ksh"+ deliver);
+        alert("The total price will be KSH "+ total);
+
+
+       
+    }
 
